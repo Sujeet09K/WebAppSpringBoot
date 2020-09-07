fe6383bcff5c2186c533bf7ac72453c9c4e000f6
Sujeet09K <teejus09@gmail.com>
2020-09-07 00:02:00 -0400

    First commit for web App spring boot project

---
+++ src/main/java/com/suj/webapp/WebAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.suj.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringBootApplication.class, args);
+	}
+
+}
